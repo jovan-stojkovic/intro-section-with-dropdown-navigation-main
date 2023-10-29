20e7fbd9cbdfaa283083359f966bbae2fa546296
jovan-stojkovic <code.jovanstojkovic@gmail.com>
2023-10-30 00:51:14 +0100

    Desktop Version Done!

--- app.js
+++ app.js
@@ -6,6 +6,18 @@ const featuresArrow = document.querySelector("#features-arrow");
 
 featuresButton.addEventListener("click", () => {
   featuresDropdown.classList.toggle("active-hidden-features");
-  featuresArrow.classList.toggle("arrow-up");
-  featuresArrow.classList.toggle("arrow-down");
+  featuresArrow.classList.toggle("arrow-up-features");
+  featuresArrow.classList.toggle("arrow-down-features");
+});
+
+// COMPANY DROPDOWN
+
+const companyButton = document.querySelector("#company");
+const companyDropdown = document.querySelector("#company-dropdown");
+const companyArrow = document.querySelector("#company-arrow");
+
+companyButton.addEventListener("click", () => {
+  companyDropdown.classList.toggle("active-hidden-company");
+  companyArrow.classList.toggle("arrow-up-company");
+  companyArrow.classList.toggle("arrow-down-company");
 });
